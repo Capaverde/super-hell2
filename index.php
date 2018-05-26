ded94c12920187f9b008be41d4a6069843f61f48
Capaverde <dudu_capaverde@terra.com.br>
2018-05-26 12:32:05 -0300

    a commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Breakthrough Society - Keepin' it free</title>
 </head>
 <body>
-<p><a href="/files/">/files/</a> - A mirror for various files found freely on the surface web</p>
-<p><a href="/b/">/b/</a> - A completely unmoderated textboard, open to all, where free speech truly reigns</p>
+<p><a href="./files/">/files/</a> - A mirror for various files found freely on the surface web</p>
+<p><a href="./b/">/b/</a> - A completely unmoderated textboard (TODO)</p>
 </body>
 </html>
